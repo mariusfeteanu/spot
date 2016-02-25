5b27d15de042ba164c520f4e8c9008a7add2a3e3
marius <marius.feteanu@mydrivesolutions.com>
2016-02-25 15:00:52 +0000

    draft version of sqlite state storage

--- src/main/scala/com/spotai/state/SQLBotContext.scala
+++ src/main/scala/com/spotai/state/SQLBotContext.scala
@@ -1,8 +1,12 @@
 package com.spotai
 package state
 
+import scala.language.postfixOps // This is so we can write 30 seconds for duration
+import scala.concurrent.{Future, Await}
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
 
+import slick.backend.DatabasePublisher
 import slick.driver.SQLiteDriver.api._
 import slick.lifted.ProvenShape.proveShapeOf
 
@@ -11,13 +15,13 @@ class SQLBotContext(dbConnectionString:String,
                     botInstanceId:String) extends BotContext {
 
 
-  class BotLastResponse(tag: Tag) extends Table[(String, String)](tag, "bot_last_respons") {
+  class BotLastResponse(tag: Tag) extends Table[(String, String)](tag, "bot_last_response") {
       def id = column[String]("bot_id", O.PrimaryKey)
       def lastResponse = column[String]("last_response")
       def * = proveShapeOf(id, lastResponse)
     }
 
-  val botLastResponse = (TableQuery[BotLastResponse]).filter(_.id === botInstanceId)
+  val botLastResponse = (TableQuery[BotLastResponse])
 
   class Predicate(tag: Tag) extends Table[(String, String, String)](tag, "bot_predicate") {
       def id = column[String]("bot_id", O.PrimaryKey)
@@ -26,23 +30,34 @@ class SQLBotContext(dbConnectionString:String,
       def * = proveShapeOf(id, predicateName, predicateValue)
     }
 
-  val predicate = (TableQuery[Predicate]).filter(_.id === botInstanceId)
+  val predicate = (TableQuery[Predicate])
 
-  DBIO.seq((botLastResponse.schema ++ predicate.schema).create)
+  val db = Database.forConfig("botSQL")
+  try {
+    val setupAction = DBIO.seq((botLastResponse.schema ++ predicate.schema).create)
+    val setupFuture: Future[Unit] = db.run(setupAction)
+    Await.result(setupFuture, 30 seconds)
+  } finally db.close
 
   override def lastResponse:Option[String] = {
     val db = Database.forConfig("botSQL")
     try {
-      val q = botLastResponse.map(_.lastResponse).take(1)
+      val q = botLastResponse.filter(_.id === botInstanceId).map(_.lastResponse).take(1)
       val action = q.result
       val result = db.run(action)
       val returned = action.statements.headOption
       returned
-    } finally {
-      db.close
-    }
+    } finally db.close
   }
+
   override def lastResponse_=(lastResponse:Option[String]) = {
+    val db = Database.forConfig("botSQL")
+    try {
+      lastResponse match {
+        case Some(someResponse:String) => botLastResponse.insertOrUpdate((botInstanceId, someResponse))
+        case None => botLastResponse.filter(_.id === botInstanceId).delete
+      }
+    } finally db.close
   }
 
   override def predicates:Map[String,String] = {
